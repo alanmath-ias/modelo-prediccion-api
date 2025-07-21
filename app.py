b9b5ba1d22c35caf9ce4b00713ef965bce6f11e1
Alan Math <alanmatiasvilla1000@gmail.com>
2025-07-21 00:05:37 -0500

    Ajuste URL para Railway

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
+import os
 
 # Cargar modelo entrenado
 modelo = joblib.load('modelo_random_forest.pkl')
@@ -34,4 +35,5 @@ def predecir():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Railway si existe
+    app.run(host='0.0.0.0', port=port)
