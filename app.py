a29d882f3463963821f28223d636e31c77d98387
Alan Math <alanmatiasvilla1000@gmail.com>
2025-07-21 00:17:26 -0500

    Segundo ajuste de URL ahora al puerto 8080

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def predecir():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Railway si existe
+    port = int(os.environ.get("PORT", 8080))  # Usa 8080 como puerto por defecto
     app.run(host='0.0.0.0', port=port)
