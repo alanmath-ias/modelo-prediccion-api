9135edf6479b459fa2afdeb2bd33d53a3a4e1dda
Alan Math <alanmatiasvilla1000@gmail.com>
2025-08-07 10:34:57 -0500

    Cambio Modelo de Predicción al puerto 8081

--- app.py
+++ app.py
@@ -37,5 +37,5 @@ def predecir():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8080))
+    port = int(os.environ.get("PORT", 8081))
     app.run(host='0.0.0.0', port=port)
